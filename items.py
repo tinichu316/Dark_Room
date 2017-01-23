1d849931dfa92a6d4cd377ff59c5e696cdbb0c11
tinichu316 <williamq.wang@mail.utoronto.ca>
2017-01-22 23:07:48 -0500

    minor tweaks to strings

--- items.py
+++ items.py
@@ -120,7 +120,7 @@ class Trinket(Item):
         if self.isFirstTime:
             player.sanity += self.sanityAmt
             self.isFirstTime = False
-            print("You place the trinket around your neck and feel better protected. \nYour sanity has increased slightly.")
+            print("You place the trinket around your neck and feel better protected. \nYour sanity has increased slightly.\n")
 
 
 
